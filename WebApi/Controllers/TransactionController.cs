89e098939ee30d2d5ff306022fdc549d8376d9ac
Breno Luiz Braga <blbraga@gmail.com>
2017-09-23 09:51:16 -0300

    Start do projeto

---
+++ WebApi/Controllers/TransactionController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+    }
+}
