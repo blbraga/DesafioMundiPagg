830b9756030fe965db9ac19df721b8d3919e0901
Breno Luiz Braga <blbraga@gmail.com>
2017-09-26 12:42:12 -0300

    Subindo novamente o wwwroot

---
+++ FrontEnd/wwwroot/js/jsApi.js
@@ -0,0 +1,22 @@
+function jsApi() {
+};
+
+jsApi.postInformation = function (method, url, information, fnCallback) {
+    if (!$.isFunction(fnCallback))
+        return false;
+
+    $.ajax({
+        type: method,
+        url: url,
+        data: information,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: true,
+        success: function (data) {
+            fnCallback(data);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert('Ocorreu um erro interno não esperado.');
+        }
+    });
+};
