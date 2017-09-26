1dc2fe6ce1dbd3e8188af0fb0ee810b08a05ec6f
Breno Luiz Braga <blbraga@gmail.com>
2017-09-26 11:53:36 -0300

    Correção de delete.

---
+++ FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontEnd.Models;
+using Microsoft.Extensions.Options;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppSettingsModel _appSettings;
+
+        public HomeController(IOptions<AppSettingsModel> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MerchantKey = _appSettings.MerchantKey;
+            ViewBag.WebApi = _appSettings.WebApi;
+
+            return View();
+        }
+    }
+}
